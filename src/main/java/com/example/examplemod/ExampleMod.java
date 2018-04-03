f171e168585923f6c844da43bb3de076cb61cdd8
WuLiangMeiYue <604675028@qq.com>
2018-04-03 13:44:01 +0800

    应群里要求  增加.gitignore文件

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,16 +5,23 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
+public class ExampleMod {
+//    modID
+    public static final String MODID = "oopc";
+//    mod版本
     public static final String VERSION = "1.0";
-    
+//    mod名称
+    public static final String modName = "Origin Of Planet";
+//    mod开发者名单
+    public List authorList = new ArrayList();
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
     }
 }
